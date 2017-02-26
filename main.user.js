16c78b3e174a27e4cb5f398cea7360ff14deb266
Zsuró Tibor <zsurotibor@gmail.com>
2017-02-26 18:04:38 +0100

    Add first version.

---
+++ main.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name        TrophyFincancePlus
+// @namespace   http://trophymanager.com
+// @description More fincance information for Trophymanager.
+// @include     http://trophymanager.com/matches/*
+// @include     https://trophymanager.com/matches/*
+// @author    	Dgzt
+// @version     0.1
+// @grant       none
+// ==/UserScript==
+
+var tfp_wait_msec=200;
+var tfp_ticket_price=200;
+
+// TODO Rewrite to JQuery's change function
+this.watch('end_of_game', function(prop, oldvarl, newval){
+  
+  // Wait for reload stadion section
+  setTimeout(function(){
+    var attendance = parseInt(match_info["attendance"]);
+    
+    var attendanceSpan = $(".attendance");
+    var ul = attendanceSpan.parent().parent();
+    ul.append('<li>' +
+              '<span class="bold">Income:</span><br/>' +
+              '<span>Stadion: </span> <span>' + number_format(attendance*tfp_ticket_price) + '</span>' +
+              '</li>');
+  }, tfp_wait_msec);
+  
+  return newval;
+});
