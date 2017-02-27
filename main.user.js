ad2f947744de75d10e34fca263654bdb64fd917b
Zsuró Tibor <zsurotibor@gmail.com>
2017-02-27 19:22:21 +0100

    Rewrite to MutationObserver

--- main.user.js
+++ main.user.js
@@ -5,27 +5,38 @@
 // @include     http://trophymanager.com/matches/*
 // @include     https://trophymanager.com/matches/*
 // @author    	Dgzt
-// @version     0.2
+// @version     0.3
 // @grant       none
 // ==/UserScript==
 
-var tfp_wait_msec=200;
 var tfp_ticket_price=200;
 
-// TODO Rewrite to JQuery's change function
-this.watch('end_of_game', function(prop, oldvarl, newval){
+$(document).ready(function(){
+  function mutationHandler (mutationRecords){
+    mutationRecords.forEach(function(mutation){
+      
+      if(mutation.type==='childList' && mutation.addedNodes.length > 0){
+        mutation.addedNodes.forEach(function(node){
+          if($(node).hasClass('post_report')){
+            var attendanceSpan = $(node).find('.attendance');
+            var attendance = parseInt(match_info.attendance);
+            var ul = $(attendanceSpan).parent().parent();
+            ul.append('<li>' +
+                    '<span class="bold">Income:</span><br/>' +
+                    '<span>Stadium: </span> <span>' + number_format(attendance*tfp_ticket_price) + '</span>' +
+                    '</li>');
+          }
+        });
+      }
+    });
+  }
   
-  // Wait for reload stadion section
-  setTimeout(function(){
-    var attendance = parseInt(match_info["attendance"]);
-    
-    var attendanceSpan = $(".attendance");
-    var ul = attendanceSpan.parent().parent();
-    ul.append('<li>' +
-              '<span class="bold">Income:</span><br/>' +
-              '<span>Stadium: </span> <span>' + number_format(attendance*tfp_ticket_price) + '</span>' +
-              '</li>');
-  }, tfp_wait_msec);
+  var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+  var myObserver = new MutationObserver (mutationHandler);
+  var obsConfig = { childList: true, characterData: true, attributes: true, subtree: true };
   
-  return newval;
+  var targetNodes = $(".box_body");
+  targetNodes.each(function(){
+    myObserver.observe(this, obsConfig);
+  });
 });
